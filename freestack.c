26aef53bd4b6d4ca6252f444dd7a57ae35cef926
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-12 22:22:27 +0000

    free stack

---
+++ freestack.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * free_stack_t - free's a stack
+ * @stack - stack to free
+ * Return: void
+ */
+
+void free_stack_t(stack_t *stack)
+{
+	stack_t *current = stack;
+
+	while (stack != NULL)
+	{
+		current = stack;
+		stack = stack->next;
+		free(current);
+	}
+}
