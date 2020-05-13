4e9ad63627d6b08ac60b5ae57bbeefc5228a8990
Sean Taylor <silverwolf4350@gmail.com>
2020-05-13 22:45:20 +0000

    Fixed negatives bug.

--- bytecodes.m
+++ bytecodes.m
@@ -1,19 +1,100 @@
-push 19
-push 2
-add
-pint
-push 3
-sub
-pint
-push 4
-mul
-push 0
-div
-push 6
-mod
-push 7
-swap
-push 8
-nop
-pop
+
+
+push 678
+
+pall
+push 995
+
+
+push 591
+pall
+push 607
+push -392
+pall
+push 887
+push -607
+pall
+push -481
+push -227
+push -728
+pall
+pall
+
+pall
+
+push 272
+
+pall
+
+
+push -97
+push -278
+pall
+
+push -514
+
+push -864
+
+push -336
+
+
+pall
+
+pall
+pall
+pall
+
+pall
+
+pall
+
+
+pall
+
+
+
+
+
+pall
+pall
+pall
+pall
+pall
+pall
+push -888
+push -217
+push -122
+pall
+push 857
+push -155
+
+pall
+push -963
+pall
+push 480
+push 11
+pall
+
+pall
+
+push 813
+pall
+pall
+
+push -533
+pall
+pall
+pall
+push -827
+
+push 676
+push -22
+pall
+
+
+
+
+
+pall
+
 pall
