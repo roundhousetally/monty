28a40757e012ef6d955cd79736354c641fd38739
Sean Taylor <silverwolf4350@gmail.com>
2020-05-13 21:26:40 +0000

    Added empty stack check on pint.

--- pint.c
+++ pint.c
@@ -3,11 +3,17 @@
 /**
  * pint - prints the value at the top of the stack
  * @stack: stack top
- * @ln: att unused
+ * @ln: line number
  * Return: void
  */
 
-void pint(stack_t **stack, const unsigned int ln __attribute__((unused)))
+void pint(stack_t **stack, unsigned int ln)
 {
+	if (*stack == NULL)
+	{
+		freeall(*stack, (char *)args[0], (FILE *)args[1], *((int *)args[2]));
+		dprintf(2, "L%d: can't pint, stack empty\n", ln);
+		exit(EXIT_FAILURE);
+	}
 	printf("%d\n", (*stack)->n);
 }
